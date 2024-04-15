43450a137d2478837d8796060bbeab71e0b079bd
Gingershine <142210638+Gingershine@users.noreply.github.com>
2024-04-15 10:43:31 +0300

    last fixing

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -18,7 +18,7 @@ const MovieDetailsPage = () => {
   const [movieData, setMovieData] = useState(null);
   const [error, setError] = useState(false);
   const location = useLocation();
-  const backLinkRef = useRef(location.state?.from ?? "/");
+  const backLinkRef = useRef(location.state ?? "/");
   
   const defaultImg = 'https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg'
 
